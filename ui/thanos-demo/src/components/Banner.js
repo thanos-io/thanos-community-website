d0c55612500f3f3cea1ade0eb50fc120370e0817
tend2infinity <somu12.ss@gmail.com>
2022-05-28 22:16:36 +0530

    website ui init
    Signed-off-by: tend2infinity <somu12.ss@gmail.com>

---
+++ ui/thanos-demo/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+function Banner() {
+
+    return (
+        <div style={{display:'flex',flexDirection:'column', alignItems:'center'}}>
+            Thanos
+            <div>Open source highly available prometheus setup with long term storage capabilities</div>
+        </div>
+     );
+}
+
+export default Banner;
