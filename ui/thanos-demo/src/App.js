29509fb4c1c1dbba18165639a58170c51bd2a8f8
tend2infinity <somu12.ss@gmail.coom>
2022-05-29 02:05:20 +0530

    added banners and cutlets
    Signed-off-by: tend2infinity <somu12.ss@gmail.coom>

--- ui/thanos-demo/src/App.js
+++ ui/thanos-demo/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Cutlet from './components/Cutlet';
+import {cutletArray} from './utilities/CutletText'
 import './App.css'
 function App() {
+  console.log(cutletArray)
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <div style={{display:'flex'}}>
+      {
+        cutletArray.map((item,index) => {
+          return <Cutlet key={index} image={item.image} text={item.text} imageName={item.imageName} />
+        })
+      }
+      </div>
     </div>
   );
 }
