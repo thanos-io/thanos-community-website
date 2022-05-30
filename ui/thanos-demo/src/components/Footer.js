71d8bf99224064ee5d01a219242a48591e528e2d
tend2infinity <somu12.ss@gmail.coom>
2022-05-30 10:19:35 +0530

    added branding and footer
    Signed-off-by: tend2infinity <somu12.ss@gmail.coom>

---
+++ ui/thanos-demo/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+function Footer() {
+    return (
+        <div style={{display:'flex',justifyContent:'center'}}>
+            <div style={{backgroundColor:'#F5F5F5', display:'flex', flexDirection:'column',alignItems:'center', padding: "5rem 1rem 2rem 1rem" ,flexBasis:'100%'}}>
+                <img src="https://thanos.io/cloud-native-computing.svg" alt="Image" style={{width:'25rem',marginBottom:'1rem'}} />
+                <Typography variant="body">We are a Cloud Native Computing Foundation Incubating project.</Typography>
+            </div>
+            <div style={{backgroundColor:'#F5F5F5', display:'flex', flexDirection:'column',alignItems:'center', padding: "2rem 1rem" ,flexBasis:'100%'}}>
+                <Typography variant="body">Powered By</Typography>
+                <img src="https://thanos.io/logos/civo.png" alt="Image" style={{width:'25rem',marginBottom:'1rem'}} />
+            </div>
+        </div>
+
+      );
+}
+
+export default Footer;
