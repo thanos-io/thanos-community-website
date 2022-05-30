ea5aec1fb9aedb42742f2032136a3962c5f719d8
AAYUSH SHARMA <aayushsharma@AAYUSHs-MacBook-Air.local>
2022-05-30 23:18:22 +0530

    added the link to the civo image

--- ui/thanos-demo/src/components/Footer.js
+++ ui/thanos-demo/src/components/Footer.js
@@ -1,18 +1,48 @@
 import { Typography } from "@mui/material";
 function Footer() {
-    return (
-        <div className="wrapping">
-            <div style={{ backgroundColor: '#F5F5F5', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: "5rem 1rem 2rem 1rem", flexBasis: '100%' }}>
-                <img src="https://thanos.io/cloud-native-computing.svg" alt="CNCF Logo" style={{ width: '25rem', marginBottom: '1rem' }} />
-                <Typography variant="body">We are a Cloud Native Computing Foundation Incubating project.</Typography>
-            </div>
-            <div style={{ backgroundColor: '#F5F5F5', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: "2rem 1rem", flexBasis: '100%' }}>
-                <Typography variant="body">Powered By</Typography>
-                <img src="https://thanos.io/logos/civo.png" alt="Civo Logo" style={{ width: '25rem', marginBottom: '1rem' }} />
-            </div>
-        </div>
-
-    );
+  return (
+    <div className="wrapping">
+      <div
+        style={{
+          backgroundColor: "#F5F5F5",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "5rem 1rem 2rem 1rem",
+          flexBasis: "100%",
+        }}
+      >
+        <a href="https://www.civo.com">
+          {" "}
+          <img
+            src="https://thanos.io/cloud-native-computing.svg"
+            alt="CNCF Logo"
+            style={{ width: "25rem", marginBottom: "1rem" }}
+          />
+        </a>
+        <Typography variant="body">
+          We are a Cloud Native Computing Foundation Incubating project.
+        </Typography>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#F5F5F5",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "2rem 1rem",
+          flexBasis: "100%",
+        }}
+      >
+        <Typography variant="body">Powered By</Typography>
+        <img
+          src="https://thanos.io/logos/civo.png"
+          alt="Civo Logo"
+          style={{ width: "25rem", marginBottom: "1rem" }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
